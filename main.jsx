256df61a42d1ecacf666d015ddb40334f79a5d13
PugazharasanC <pugazharasankct@gmail.com>
2020-08-01 01:12:28 +0530

    React One

---
+++ main.jsx
@@ -0,0 +1,34 @@
+var Counter = React.createClass({
+  render: function () {
+    return (
+      <button onClick={this.props.handleClick(this.props.increment)}>
+        {this.props.increment}
+      </button>
+    );
+  },
+});
+var Result = React.createClass({
+  render: function () {
+    return <p>{this.props.counterValue}</p>;
+  },
+});
+var Main = React.createClass({
+  getInitialState: function () {
+    return { counter: 0 };
+  },
+  handleClick: function (value) {
+    this.setState({ counter: this.state.counter + value });
+  },
+  render: function () {
+    return (
+      <div>
+        <Result counterValue={this.state.counter} />
+        <Counter handleClick={this.handleClick} increment={1} />
+        <Counter handleClick={this.handleClick} increment={2} />
+        <Counter handleClick={this.handleClick} increment={4} />
+        <Counter handleClick={this.handleClick} increment={8} />
+      </div>
+    );
+  },
+});
+React.render(<Main />, document.getElementById("root"));
